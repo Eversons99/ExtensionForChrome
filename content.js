2ac7ca4ae57c203f15d341203ef8f85a7ebc134c
Everson Alves <everson.santoss1999@outlook.com>
2022-04-11 17:54:01 -0300

    Update cssa and interaction with the button

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
 //Roda no background do  navegador
 
 //Recebe a mensagem do script do popup
-chrome.runtime.onMessage.addListener(function(request) {
-    console.log(request.sns)
-    let allSNs = request.sns
-    
-    if(allSNs.length == 0) return alert("Nenhum dado foi inserido")
-    
-    inputSN(allSNs)
-})
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        sendResponse({contentBtn: "Processando..."})
+        let allSNs = request.sns
+        inputSN(allSNs)
+    }
+);
 
 //Pega tabela com o registros
 function getTable(){
@@ -19,9 +19,10 @@ function getTable(){
 }
   
 //insere os SNS e salva
-async function inputSN(splitSN){
-  
-    const arraySns = splitSN
+async function inputSN(allSNs){
+    
+    loadingAnimation()
+    const arraySns = allSNs
   
     let totalOfPages = Math.ceil(arraySns.length / 30)
     
@@ -36,9 +37,9 @@ async function inputSN(splitSN){
                 const serialNumber = document.getElementById('serial_fornecedor')
                 await sleep(2)
                 serialNumber.value = arraySns[y] 
-                await sleep(1)
+                await sleep(2)
                 saveChange()
-                await sleep(1)
+                await sleep(2)
             }
   
             if (y >= 30){
@@ -48,6 +49,9 @@ async function inputSN(splitSN){
             }
         }  
     }
+
+    loadingAnimation(false)
+    chrome.runtime.sendMessage({newTextContent: "Nova inserção"})
 }
   
 //Salva as alterações e troca fecha a pagina
@@ -55,14 +59,13 @@ function saveChange(){
     setTimeout(() => {
         const btnSave = document.querySelector('.disab')
         //btnSave.click()
-            
-        //Fechar
+
         const btnClose = document.querySelectorAll('.fa-times')[2]
         btnClose.click()
      }, 1000)
 }
   
-//Função "Cronometro"await sleep(2)
+//Função "Cronometro"
 async function sleep(time){
     return new Promise(function (resolve, reject){
         setTimeout(() => {  
@@ -70,4 +73,48 @@ async function sleep(time){
         }, time * 1000)
          
     })
+}
+
+// Cria animação de carregamento
+function loadingAnimation(isActive = true) {
+
+    // Verifica se o parâmetro is active é true. Se sim, inicia a animação
+    if (isActive == true) {
+
+        // Salva body em uma variável, cria uma div #modal-container 
+        const body = document.querySelector('body')
+        const divModalContainer = document.createElement('div')
+        divModalContainer.setAttribute('class', 'modal-container')
+        divModalContainer.setAttribute('id', 'modal-container')
+
+        // Cria uma div dentro da #modal-container
+        const divModal = document.createElement('div')
+        divModal.setAttribute('class', 'modal')
+
+        // Cria h1
+        const loading = document.createElement('h1')
+        loading.textContent = 'Carregando resultados'
+
+        // Cria span para a moldura o quadro
+        const loader = document.createElement('span')
+        loader.setAttribute('class', 'loader')
+
+        // Cria ooutro span para preencher o quadro
+        const innerLoader = document.createElement('span')
+        innerLoader.setAttribute('class', 'loader-inner')
+
+        // Insere um no outro
+        loader.append(innerLoader)
+
+        // Insere a div dentro da div modal, e a div modal na div modal container
+        divModal.append(loading, loader)
+        divModalContainer.append(divModal)
+        body.append(divModalContainer)
+    }
+
+    // Se falso, deleta a div modal-container
+    if (isActive == false) {
+        const body = document.querySelector('body')
+        body.removeChild(document.getElementById('modal-container'))
+    }
 }
--- popup/popup.js
+++ popup/popup.js
@@ -13,22 +13,94 @@ function getSerialNumber(){
     allSNs.forEach((sn, index) => {
         if(sn == ''){
             allSNs.splice(index, 1)
-            }
+        }
     })
 
     //Encaminha uma mensagem para outra parte do meu projeto contendo o array de dados já tratado
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {sns: allSNs});
-    });
+        chrome.tabs.sendMessage(tabs[0].id, {sns: allSNs}, function(response) {
+            const contentBtn = response.contentBtn
+            awaitProcessing(contentBtn)
+          })
+    })
+
 }
+
+function awaitProcessing(contentBtn){
+    //Escondendo os botões
+    document.getElementsByTagName('textarea')[0].style.display = 'none'
+    document.getElementById('prosseguir').textContent = contentBtn
+
+    //Criando o novo botão para substituir
+    const divButton = document.querySelector('.btn')
+    const newBtn = document.createElement('button') 
+    const btnMain = document.getElementById('prosseguir')
+    newBtn.setAttribute('class', 'home-button')
+    newBtn.setAttribute('id', 'newInsert')
+    btnMain.disabled = true
+
+    chrome.runtime.onMessage.addListener(
+    function(request) {
+        const myData = request.newTextContent
+        newBtn.textContent = myData
+        divButton.append(newBtn)
+        btnMain.style.display = 'none'
+    }
+)
+
+    newBtn.addEventListener("click", ()=>{
+        const textarea = document.getElementsByTagName('textarea')[0]
+        const btnMain = document.getElementById('prosseguir')
+        btnMain.textContent = 'Prosseguir'
+        textarea.style.display = 'flex'
+        textarea.value = ''
+        btnMain.textContent = "Prosseguir"
+        btnMain.style.display = 'flex'
+        newBtn.style.display = 'none'
+        btnMain.disabled = false
+    })
+
+}
+
+
 
 /*
 Exemplo de entrada de dados
 
+7898951027901
+7898951027902
+7898951027903
+7898951027904
+7898951027905
+7898951027906
+7898951027907
+7898951027908
+7898951027909
+7898951027910
+7898951027911
+7898951027912
+7898951027913
+7898951027914
+7898951027915
+7898951027916
+7898951027917
+7898951027918
+7898951027919
+7898951027920
 7898951027921
 7898951027922
 7898951027923
 7898951027924
-7898951027925
+7898951027924
 7898951027926
+7898951027927
+7898951027928
+7898951027929
+7898951027930
+7898951027931
+7898951027932
+7898951027933
+7898951027934
+7898951027935
+7898951027936
 */
