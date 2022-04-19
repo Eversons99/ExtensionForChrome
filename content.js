0b715e62c2e6eb7b6c807447cbbc419684aafb8c
Everson <Everson.santoss1999@outlook.com>
2022-04-19 16:44:14 -0300

    Change content.js file to change params variables

--- content.js
+++ content.js
@@ -20,9 +20,15 @@ async function inputSN(allSNs, nf, equipament){
     console.log(serialNumbers, noteNumber, equipamentModel)
     //URL Principal do IXC
     const url =  'https://assinante.nmultifibra.com.br/aplicativo';
+    let descricaoEquipamento = "Roteador Huawei WIFI WS5200"
+    let notaFiscal = 24863
+    let itemsPorPagina = 29
 
+    let grid_params=  [{"TB":"almox.descricao","display":"Almoxarifado","OP":"=","P":"Almoxarifado Central","C":"AND","G":"_almox.descricao"},{"TB":"patrimonio.situacao","display":"Situação","OP":"IN","P":"\"1\",\"7\"","C":"AND","G":"_patrimonio.situacao"},{"TB":"patrimonio.descricao","display":"Descrição","OP":"=","P":`${descricaoEquipamento}`,"C":"AND","G":"_patrimonio.descricao"},{"TB":"patrimonio.numero_nf","display":"Número NF","OP":"=","P":`${notaFiscal}`,"C":"AND","G":"_patrimonio.numero_nf"}]
+
+    let grid_paramsString = JSON.stringify(grid_params)
     //Define os parametros da consulta (filtro), alterar nmr nf de acordo com a entrada do usuario 
-    let params = `page=1&rp=20${itemsPorPagina}&sortname=patrimonio.serial_fornecedor&sortorder=desc&query=&qtype=patrimonio.id&oper=L&grid_param=%5B%7B%22TB%22%3A%22almox.descricao%22%2C%22display%22%3A%22Almoxarifado%22%2C%22OP%22%3A%22%3D%22%2C%22P%22%3A%22Almoxarifado+Central%22%2C%22C%22%3A%22AND%22%2C%22G%22%3A%22_almox.descricao%22%7D%2C%7B%22TB%22%3A%22patrimonio.situacao%22%2C%22display%22%3A%22Situa%C3%A7%C3%A3o%22%2C%22OP%22%3A%22IN%22%2C%22P%22%3A%22%5C%221%5C%22%2C%5C%227%5C%22%22%2C%22C%22%3A%22AND%22%2C%22G%22%3A%22_patrimonio.situacao%22%7D%2C%7B%22TB%22%3A%22patrimonio.descricao%22%2C%22display%22%3A%22Descri%C3%A7%C3%A3o%22%2C%22OP%22%3A%22%3D%22%2C%22P%22%3A%22${descricaoEquipamento}Roteador+Huawei+WIFI+WS5200%22%2C%22C%22%3A%22AND%22%2C%22G%22%3A%22_patrimonio.descricao%22%7D%2C%7B%22TB%22%3A%22patrimonio.numero_nf%22%2C%22display%22%3A%22N%C3%BAmero+NF%22%2C%22OP%22%3A%22%3D%22%2C%22P%22%3A%2224863%22%2C%22C%22%3A%22AND%22%2C%22G%22%3A%22_patrimonio.numero_nf${numeroNF}%22%7D%5D&grid_param2=false&display=C%C3%B3digo`
+    let params = `page=1&rp=${itemsPorPagina}&sortname=patrimonio.valor_bem&sortorder=desc&query=&qtype=patrimonio.id&oper=L&grid_param=${grid_paramsString}&grid_param2=false&display=Código`
 
     //Faz a consulta com o filtro 
     const optRows = {
